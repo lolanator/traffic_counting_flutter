60ae603d38d410c5b42869c731f7572b0a14b4c5
Lola Nolan <lolanolan@Lolas-MacBook-Pro.local>
2021-03-22 16:44:29 +0000

    navigation to dashbaord form camera screen

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -8,6 +8,26 @@ class DashBoard extends StatefulWidget {
 class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.1, 0.4, 0.7, 0.9],
+                colors: [
+                  Color(0xFF3594DD),
+                  Color(0xFF4563DB),
+                  Color(0xFF5036D5),
+                  Color(0xFF5B16D0),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
