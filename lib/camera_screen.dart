c5ec82ffa9cccd435350895c1feb81bd3fac4216
Jack Cleary <jackcleary@macbook-air.home>
2021-03-29 15:36:47 +0100

    Camera Screen attempt

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -1,17 +1,95 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:camera/camera.dart';
+
+//My attempt to access the camera in the application --- Jack
+
+// A screen that takes in a list of cameras and the Directory to store images.
 class CameraScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const CameraScreen({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
+  @override
+  CameraScreenState createState() => CameraScreenState();
+}
+
+class CameraScreenState extends State<CameraScreen> {
+  // Add two variables to the state class to store the CameraController and
+  // the Future.
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Fill this out in the next steps.
+  }
+}
+
+//Below is my original attempt to access the camera --- Jack
+
+/*class CameraScreen extends StatefulWidget {
   @override
   _CameraScreenState createState() => _CameraScreenState();
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+
+  File _videoStream;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getVideo(source: ImageSource.camera);
+
+    setState(() {
+        _videoStream = File(pickedFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.yellow,
+      appBar: AppBar(
+        title: Text('Video Camera'),
+      ),
+      body: Center(
+        child: _videoStream == null
+            ? Text('No image selected.')
+            : Image.file(_videoStream),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
 }
+ */
