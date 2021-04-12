5acb6d4fc496c5d0f6dd89f5b4b0b03b7e563c82
FrontRowWithj <adebusum@tcd.ie>
2021-04-12 17:36:48 +0100

    Correct graph data

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -16,8 +16,7 @@ class _DashBoardState extends State<DashBoard>
   BuildContext _context;
   Animation<double> _animation;
   AnimationController _controller;
-  List<int> _points = [10, 2, 0, 3];
-  int strokes = 12;
+  List<int> _points = [10, 2, 0, 1];
   double _t = 0;
   List<ui.Image> _images = [null, null, null, null];
   int _vehicleNo = 0;
@@ -104,10 +103,7 @@ class _DashBoardState extends State<DashBoard>
                   width: vw(.8),
                   child: CustomPaint(
                     painter: GraphPainter.drawBarChart(
-                        _points, strokes, _t, currVehicle, _images, _vehicleNo),
-
-                    // GraphPainter(points, strokes, _t, _bezierpoints,
-                    //     _titles[_titleIndex], _chartTimes),
+                        _points, 12, _t, currVehicle, _images, _vehicleNo),
                     size: Size(vw(.9), vh(.5)),
                   ),
                 ),
